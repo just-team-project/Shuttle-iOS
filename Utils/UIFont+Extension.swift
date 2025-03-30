c45b2db1da993b4b9eeeaf60ed1146acdf37cb7d
kanghun1121 <kanghun1121@naver.com>
2025-03-30 23:09:46 +0900

    feat: UIFont 타입 메소드 생성

---
+++ Utils/UIFont+Extension.swift
@@ -0,0 +1,46 @@
+//
+//  UIFont+Extension.swift
+//  Shuttle_iOS
+//
+//  Created by 강대훈 on 3/30/25.
+//
+
+import UIKit
+
+extension UIFont {
+    static func pretendardBlack(size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-Black", size: size) ?? UIFont()
+    }
+    
+    static func pretendardBold(size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-Bold", size: size) ?? UIFont()
+    }
+    
+    static func pretendardExtraBold(size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-ExtraBold", size: size) ?? UIFont()
+    }
+    
+    static func pretendardExtraLight(size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-ExtraLight", size: size) ?? UIFont()
+    }
+    
+    static func pretendardLight(size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-Light", size: size) ?? UIFont()
+    }
+    
+    static func pretendardMedium(size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-Medium", size: size) ?? UIFont()
+    }
+    
+    static func pretendardRegular(size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-Regular", size: size) ?? UIFont()
+    }
+    
+    static func pretendardSemiBold(size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-SemiBold", size: size) ?? UIFont()
+    }
+    
+    static func pretendardThin(size: CGFloat) -> UIFont {
+        return UIFont(name: "Pretendard-Thin", size: size) ?? UIFont()
+    }
+}
