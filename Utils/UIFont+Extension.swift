dfe0a4434b6c38c2f1278183cbed2a6b6049ceaa
kanghun1121 <kanghun1121@naver.com>
2025-03-31 11:50:52 +0900

    refactor: Font 생성자 닐 코어레싱 수정

--- Utils/UIFont+Extension.swift
+++ Utils/UIFont+Extension.swift
@@ -7,40 +7,41 @@
 
 import UIKit
 
+// TODO: - 추후에 에러처리로 바꿔야할지 고민중.
 extension UIFont {
     static func pretendardBlack(size: CGFloat) -> UIFont {
-        return UIFont(name: "Pretendard-Black", size: size) ?? UIFont()
+        return UIFont(name: "Pretendard-Black", size: size) ?? UIFont.systemFont(ofSize: size)
     }
     
     static func pretendardBold(size: CGFloat) -> UIFont {
-        return UIFont(name: "Pretendard-Bold", size: size) ?? UIFont()
+        return UIFont(name: "Pretendard-Bold", size: size) ?? UIFont.systemFont(ofSize: size)
     }
     
     static func pretendardExtraBold(size: CGFloat) -> UIFont {
-        return UIFont(name: "Pretendard-ExtraBold", size: size) ?? UIFont()
+        return UIFont(name: "Pretendard-ExtraBold", size: size) ?? UIFont.systemFont(ofSize: size)
     }
     
     static func pretendardExtraLight(size: CGFloat) -> UIFont {
-        return UIFont(name: "Pretendard-ExtraLight", size: size) ?? UIFont()
+        return UIFont(name: "Pretendard-ExtraLight", size: size) ?? UIFont.systemFont(ofSize: size)
     }
     
     static func pretendardLight(size: CGFloat) -> UIFont {
-        return UIFont(name: "Pretendard-Light", size: size) ?? UIFont()
+        return UIFont(name: "Pretendard-Light", size: size) ?? UIFont.systemFont(ofSize: size)
     }
     
     static func pretendardMedium(size: CGFloat) -> UIFont {
-        return UIFont(name: "Pretendard-Medium", size: size) ?? UIFont()
+        return UIFont(name: "Pretendard-Medium", size: size) ?? UIFont.systemFont(ofSize: size)
     }
     
     static func pretendardRegular(size: CGFloat) -> UIFont {
-        return UIFont(name: "Pretendard-Regular", size: size) ?? UIFont()
+        return UIFont(name: "Pretendard-Regular", size: size) ?? UIFont.systemFont(ofSize: size)
     }
     
     static func pretendardSemiBold(size: CGFloat) -> UIFont {
-        return UIFont(name: "Pretendard-SemiBold", size: size) ?? UIFont()
+        return UIFont(name: "Pretendard-SemiBold", size: size) ?? UIFont.systemFont(ofSize: size)
     }
     
     static func pretendardThin(size: CGFloat) -> UIFont {
-        return UIFont(name: "Pretendard-Thin", size: size) ?? UIFont()
+        return UIFont(name: "Pretendard-Thin", size: size) ?? UIFont.systemFont(ofSize: size)
     }
 }
