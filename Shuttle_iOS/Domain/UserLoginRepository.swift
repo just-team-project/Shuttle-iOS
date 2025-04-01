b276baa40a2305a1c74201b9181cbe3b668aef38
kanghun1121 <kanghun1121@naver.com>
2025-04-01 17:14:31 +0900

    feat: DIContainer 의존성 주입

--- Shuttle_iOS/Domain/UserLoginRepository.swift
+++ Shuttle_iOS/Domain/UserLoginRepository.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-protocol UserLoginRepository {
+protocol UserLoginRepository: Sendable {
     func existLocalData(email: String, uuid: String) -> Bool
     func existServerData(email: String, uuid: String) throws -> Bool
     func sendData(email: String, uuid: String) throws
