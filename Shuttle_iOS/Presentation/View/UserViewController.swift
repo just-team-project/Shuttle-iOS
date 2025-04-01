8d081a4b5a4c27f335467e040b19d0e8801ac49f
kanghun1121 <kanghun1121@naver.com>
2025-04-01 20:06:44 +0900

    feat: 유저 뷰 생성

---
+++ Shuttle_iOS/Presentation/View/UserViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UserViewController.swift
+//  Shuttle_iOS
+//
+//  Created by 강대훈 on 4/1/25.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
