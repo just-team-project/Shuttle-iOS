ec3034aa6c84e9fed5c2eaede6e36e454d61fa3e
kanghun1121 <kanghun1121@naver.com>
2025-04-01 16:28:28 +0900

    feat: DIContainer 생성

---
+++ Shuttle_iOS/Utils/DIContainer.swift
@@ -0,0 +1,23 @@
+//
+//  DIContainer.swift
+//  Shuttle_iOS
+//
+//  Created by 강대훈 on 4/1/25.
+//
+
+public actor DIContainer {
+    private var dictionaries : [String:Any] = [:]
+    
+    public func register<T>(_ key: T.Type, _ value: T) {
+        dictionaries[String(describing: key)] = value
+    }
+    
+    public func resolve<T>(key: T.Type) throws -> T {
+        let key = String(describing: key)
+        guard let value = dictionaries[key] as? T else {
+            // TODO: - 에러 핸들링 생각해봐야 함.
+            throw CancellationError()
+        }
+        return value
+    }
+}
