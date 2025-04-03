d26b6bf71a3ebd1b3717314d447898ce0bab4627
kanghun1121 <kanghun1121@naver.com>
2025-04-03 18:34:04 +0900

    refactor: 에러 처리 코드 개선

--- Shuttle_iOS/App/SceneDelegate.swift
+++ Shuttle_iOS/App/SceneDelegate.swift
@@ -26,8 +26,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             try await registerViewModel()
             try await registerViewController()
         }
-        catch let error {
-            print((error as? DIError)?.description)
+        catch let error as DIError {
+            print(error.description)
+        } catch {
+            print(error.localizedDescription)
         }
     }
     
