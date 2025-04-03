192fd5d7f22e34d81f8190c92248755d688137a3
kanghun1121 <kanghun1121@naver.com>
2025-04-03 13:08:36 +0900

    feat: 클릭 이벤트 정의

--- Shuttle_iOS/Presentation/ViewModel/UserViewModel.swift
+++ Shuttle_iOS/Presentation/ViewModel/UserViewModel.swift
@@ -6,7 +6,53 @@
 //
 
 import Foundation
+import Combine
 
 final class UserViewModel {
+    enum Input {
+        case logoutTapped
+        case faqTapped
+        case alarmTapped
+        case notificationTapped
+    }
     
+    enum Output {
+        
+    }
+    
+    private let output = PassthroughSubject<Output, Never>()
+    private var cancellables: Set<AnyCancellable> = .init()
+    
+    func transform(input: AnyPublisher<Input, Never>) -> AnyPublisher<Output, Never> {
+        input.sink { [weak self] event in
+            switch event {
+            case .logoutTapped:
+                self?.logoutTapped()
+            case .faqTapped:
+                self?.faqTapped()
+            case .alarmTapped:
+                self?.alarmTapped()
+            case .notificationTapped:
+                self?.notificationTapped()
+            }
+        }.store(in: &cancellables)
+        
+        return output.eraseToAnyPublisher()
+    }
+    
+    private func logoutTapped() {
+        
+    }
+    
+    private func faqTapped() {
+        
+    }
+    
+    private func alarmTapped() {
+        
+    }
+    
+    private func notificationTapped() {
+        
+    }
 }
