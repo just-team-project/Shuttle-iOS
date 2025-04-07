7bd135f70ec58f71739dfcc1af6e6cb223e9f9fd
kanghun1121 <kanghun1121@naver.com>
2025-04-07 13:50:21 +0900

    refactor: UIAlertController 메소드 수정 및 추가

--- Shuttle_iOS/Utils/UIAlertController+Extension.swift
+++ Shuttle_iOS/Utils/UIAlertController+Extension.swift
@@ -1,24 +1,47 @@
 import UIKit
 
-/// isError : true -  "확인" 버튼만 나옵니다. (default)
-/// isError : false - "확인", "취소" 버튼이 나옵니다.
 extension UIAlertController {
-    static func make(title: String = "",
-                     message: String,
-                     isError: Bool = true,
-                     defaultLabel: String = "확인",
-                     cancelLabel: String = "취소"
+    /// 오류 발생시 사용하는 Alert
+    static func errorAlert(message: String) -> UIAlertController {
+        let alertController = UIAlertController(
+            title: "",
+            message: message,
+            preferredStyle: .alert
+            )
+        
+        alertController.addAction(
+            .init(title: "확인",
+                  style: .default)
+        )
+        
+        return alertController
+    }
+    
+    static func alert(
+        title: String = "",
+        message: String = "",
+        defaultCompletion: (() -> Void)? = nil,
+        cancelCompletion: (() -> Void)? = nil
     ) -> UIAlertController {
         let alertController = UIAlertController(
             title: title,
             message: message,
             preferredStyle: .alert
-            )
+        )
         
-        alertController.addAction(.init(title: defaultLabel, style: .default))
-        if !isError {
-            alertController.addAction(.init(title: cancelLabel, style: .cancel))
+        let defaultAction = UIAlertAction(
+            title: "확인",
+            style: .default) { _ in
+            defaultCompletion?()
         }
+        
+        let cancelAction = UIAlertAction(
+            title: "취소",
+            style: .cancel) { _ in
+            cancelCompletion?()
+        }
+        alertController.addAction(defaultAction)
+        alertController.addAction(cancelAction)
         return alertController
     }
 }
