44b9de7552e3b63685378457f59d7d04a244b3c6
kanghun1121 <kanghun1121@naver.com>
2025-04-07 12:30:50 +0900

    chore: 주석 제거 및 UIKit 의존성 추가

--- Shuttle_iOS/Data/UserLogoutRepository+Impl.swift
+++ Shuttle_iOS/Data/UserLogoutRepository+Impl.swift
@@ -1,9 +1,4 @@
-//
-//  UserLogoutRepository+Impl.swift
-//  Shuttle_iOS
-//
-//  Created by 강대훈 on 4/7/25.
-//
+import UIKit
 
 final class UserLogoutRepositoryImpl: UserLogoutRepository {
     func executeLogout(uuidString: String) {
