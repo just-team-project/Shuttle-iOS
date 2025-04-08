f842bfeb42964d89e4863ad932c24bfdc20e347a
kanghun1121 <kanghun1121@naver.com>
2025-04-08 14:38:54 +0900

    chore: 파일 위치 변경

---
+++ Shuttle_iOS/Presentation/View/StationTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  StationTableViewCell.swift
+//  Shuttle_iOS
+//
+//  Created by 강대훈 on 4/8/25.
+//
+
+import UIKit
+
+class StationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Shuttle_iOS/Presentation/View/busSliderView.swift
@@ -0,0 +1,20 @@
+//
+//  busSliderView.swift
+//  Shuttle_iOS
+//
+//  Created by 강대훈 on 4/8/25.
+//
+
+import UIKit
+
+class busSliderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
