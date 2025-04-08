fb971d40f7e82d5d5e45e277e701cf618ca40502
kanghun1121 <kanghun1121@naver.com>
2025-04-08 19:18:28 +0900

    feat: Domain, Data 추가

---
+++ Shuttle_iOS/Domain/BusStation.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+struct BusStation {
+    let name: String
+    let additionalName: String?
+    let lat: Double
+    let lon: Double
+}
+
--- Shuttle_iOS/Domain/BusStationUseCase.swift
+++ Shuttle_iOS/Domain/BusStationUseCase.swift
@@ -1,3 +1,5 @@
+import UIKit
+
 final class BusStationUseCase {
     func execute(name busName: String) {
         
