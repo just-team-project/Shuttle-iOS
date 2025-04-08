b6ff4576408052b3e69bbe0969bc94f6ae10ff79
kanghun1121 <kanghun1121@naver.com>
2025-04-08 20:04:11 +0900

    feat: Repository 테스트 구현 및 실제 구현체 TODO 작성

--- Shuttle_iOS/Data/BusStationRepository+Test.swift
+++ Shuttle_iOS/Data/BusStationRepository+Test.swift
@@ -1,6 +1,12 @@
 import UIKit
 
 final class BusStationRepositoryTest: BusStationRepository {
-    
+    func readBusStations(name busName: String) throws -> [BusStation] {
+        return [
+            BusStation(name: "한신대학교 (정문)", lat: 37.1933281, lon: 127.0225987),
+            BusStation(name: "청명역 (경유)", lat: 37.2595832, lon: 127.0790738),
+            BusStation(name: "한신대학교 (후문)", lat: 37.1933281, lon: 127.0225987),
+        ]
+    }
 }
 
