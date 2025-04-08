9b69ed303e1dea15f8551fe9f362bd1ae9075e71
kanghun1121 <kanghun1121@naver.com>
2025-04-08 20:33:08 +0900

    chore: 테스트 객체 수정

--- Shuttle_iOS/Data/BusStationRepository+Test.swift
+++ Shuttle_iOS/Data/BusStationRepository+Test.swift
@@ -4,7 +4,7 @@ final class BusStationRepositoryTest: BusStationRepository {
     func readBusStations(name busName: String) throws -> [BusStation] {
         return [
             BusStation(name: "한신대학교 (정문)", lat: 37.1933281, lon: 127.0225987),
-            BusStation(name: "청명역 (경유)", lat: 37.2595832, lon: 127.0790738),
+            BusStation(name: "청명역 (경유)", additionalName: "수인분당선", lat: 37.2595832, lon: 127.0790738),
             BusStation(name: "한신대학교 (후문)", lat: 37.1933281, lon: 127.0225987),
         ]
     }
